a09e14412d2a1d3f67e26d64b06ad9e25172c1fc
Rush <johny64@gmail.com>
2014-04-13 18:09:13 +0300

    added english locale for streams infusion

---
+++ AL Streams/infusions/al_streams/locale/English.php
@@ -0,0 +1,32 @@
+<?php
+ $locale['ss1'] = "Settings";
+ $locale['ss2'] = "User group:";
+ $locale['ss3'] = "Save";
+ $locale['ss4'] = "Create channel";
+ $locale['ss5'] = "Service:";
+ $locale['ss6'] = "ID or nickname in service:";
+ $locale['ss7'] = "Description:";
+ $locale['ss8'] = "Create";
+ $locale['ss10'] = "Streams";
+ $locale['ss11'] = "<strong style=\'color\:green\;\'>Live now!</strong>";
+ $locale['ss12'] = "Watchers: ";
+ $locale['ss13'] = "<font style=\'color\:red\;\'>Offline</font>";
+ $locale['ss20'] = "Stream of ";
+ $locale['ss21'] = "To streams list";
+ $locale['ss22'] = "Edit stream";
+ $locale['ss23'] = "Save";
+ $locale['ss25'] = "User";
+ $locale['ss26'] = "Admin";
+ $locale['ss27'] = "Superadmin";
+ $locale['ss24'] = "+ Add stream";
+ $locale['ss30'] = "<strong>How to add stream:</strong><br /><strong>regame.tv</strong> ...<br /> <strong>own3d.tv</strong> get stream ID from it's link, for example http://own3d.tv/l/<strong>12345</strong> or http://www.own3d.tv/channel_name/live/<strong>12345</strong> <br /> <strong>justin.tv/twitch.tv</strong> get ID from channel's link, for example http://justin.tv/<strong>channel</strong><br /> ";
+ $locale['ss31'] = "Chat";
+ $locale['ss32'] = "Say";
+ $locale['ss33'] = "In chat: ";
+ $locale['ss37'] = " people";
+ $locale['ss34'] = "people";
+ $locale['ss35'] = "Chat only for authorized users";
+ $locale['ss36'] = "No messages.";
+ $locale['ss38'] = "Chat archive";
+ 
+?>
