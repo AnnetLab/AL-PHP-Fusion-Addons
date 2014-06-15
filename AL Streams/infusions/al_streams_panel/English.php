3e41ccd242e820ca8469546d7033a127b635640c
hotrush <johny64@gmail.com>
2014-06-15 10:09:26 +0300

    Create English.php

---
+++ AL Streams/infusions/al_streams_panel/English.php
@@ -0,0 +1,9 @@
+<?php
+
+$locale['alstrp1'] = "Streams online";
+$locale['alstrp2'] = "No streams";
+$locale['alstrp3'] = "No active streams";
+$locale['alstrp4'] = "Watch: ";
+$locale['alstrp5'] = "In chat: ";
+
+?>
