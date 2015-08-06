dfa181e1859992bdba07d51985e2cfb32d505f33
rush <johny64@gmail.com>
2015-08-06 17:24:27 +0300

    fix "my posts" page in AL Blogs

--- AL Blog/infusions/al_blog/pages/my_posts.php
+++ AL Blog/infusions/al_blog/pages/my_posts.php
@@ -2,7 +2,10 @@
 
 if (!iMEMBER) redirect(FUSION_SELF);
 
-$result = dbquery("SELECT p.*,pc.* FROM ".DB_AL_BLOG_POSTS." p LEFT JOIN ".DB_AL_BLOG_CATEGORIES." pc ON pc.alb_cat_id=p.alb_post_cat ORDER BY alb_post_datestamp DESC");
+if (!isset($_GET['rowstart']) || !isnum($_GET['rowstart'])) $_GET['rowstart'] = 0;
+
+$total_posts = dbcount("(alb_post_id)",DB_AL_BLOG_POSTS,"alb_post_user='".$userdata['user_id']."'");
+$result = dbquery("SELECT p.*,pc.* FROM ".DB_AL_BLOG_POSTS." p LEFT JOIN ".DB_AL_BLOG_CATEGORIES." pc ON pc.alb_cat_id=p.alb_post_cat WHERE p.alb_post_user='".$userdata['user_id']."' ORDER BY alb_post_datestamp DESC");
 if (dbrows($result)) {
     opentable($locale['alb20']);
     while ($data=dbarray($result)) {
@@ -12,6 +15,10 @@ if (dbrows($result)) {
             $status = $data['alb_post_status'] == 1 ? $locale['alb45'] : $locale['alb46'];
 //        }
         echo "<a href='".FUSION_SELF."?p=view_post&id=".$data['alb_post_id']."'>".$data['alb_post_title']."</a> <a href='".FUSION_SELF."?p=manage_post&id=".$data['alb_post_id']."'><img src='".AL_BLOG_DIR."asset/images/edit.png' width='16' /></a> <a href='".FUSION_SELF."?p=manage_post&delete=".$data['alb_post_id']."'><img src='".AL_BLOG_DIR."asset/images/delete.png' width='16' /></a><br />";
+
+        if ($total_posts > 10) {
+            echo "<div align='center' style=';margin-top:5px;'>\n".makepagenav($_GET['rowstart'],10,$total_posts,3,FUSION_SELF)."</div>";
+        }
     }
     closetable();
 } else {
